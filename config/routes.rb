250b6934128bea96ae91a99877bf86397250668e
TdeSouzaRRC <tdesouza@academic.rrc.ca>
2017-10-25 16:25:51 +0000

    Adjusted routes and linkto to work properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "pages#home", as: 'home'
-  GET 'about_us', to: "pages#about", as: 'about'
+  root to: "pages#home"
+  get 'home', to: "pages#home", as: "home"
+  get 'about', to: "pages#about", as: "about_us"
 end
