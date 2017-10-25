1bc12fa326d56e3d8a626de74d279e9d16fc9f30
TdeSouzaRRC <tdesouza@academic.rrc.ca>
2017-10-25 18:15:45 +0000

    Adjusted pages controller lizards action to capture parameters and changed views to use it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,8 @@ class PagesController < ApplicationController
     def about
 
     end
+
+    def lizards
+        @number_of_lizards = params[:number]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "pages#home"
+  
   get 'home', to: "pages#home", as: "home"
+  
   get 'about', to: "pages#about", as: "about_us"
+
+  get 'lizards/:number', to: "pages#lizards", as: "lizards"
 end
